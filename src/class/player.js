b7405180f125807e5c55829c47f6f5c55526535f
LeoG1904 <leoginer19@yahoo.com>
2025-11-20 10:02:51 +0100

    [AJOUT] ajout de la possibilité de sauter

--- src/class/player.js
+++ src/class/player.js
@@ -16,6 +16,12 @@ export class Player{
             right: false
         };
 
+        // Saut
+        this.velocityY = 0;      // vitesse verticale
+        this.gravity = -20;      // gravité
+        this.jumpPower = 8;      // puissance du saut
+        this.isGrounded = true;  // joueur au sol ?
+
         this.createMesh();
         this.initControls();
     }
@@ -25,7 +31,7 @@ export class Player{
         const material = new THREE.MeshStandardMaterial({ color: 0x00ffcc });
 
         this.mesh = new THREE.Mesh(geometry, material);
-        this.mesh.position.set(0, 0.5, 0);
+        this.mesh.position.set(0, 0.5, 0); // 0.5 pour que le cube repose sur le sol
 
         this.scene.add(this.mesh);
     }
@@ -51,6 +57,12 @@ export class Player{
             case "d":
                 this.direction.right = isDown;
                 break;
+            case " ": // espace pour sauter
+                if (isDown && this.isGrounded) {
+                    this.velocityY = this.jumpPower;
+                    this.isGrounded = false;
+                }
+                break;
         }
     }
 
@@ -59,9 +71,21 @@ export class Player{
 
         const move = this.speed * dt;
 
+        // Mouvement horizontal
         if (this.direction.forward) this.mesh.position.z -= move;
         if (this.direction.backward) this.mesh.position.z += move;
         if (this.direction.left) this.mesh.position.x -= move;
         if (this.direction.right) this.mesh.position.x += move;
-    }
+
+        // Gravité & saut
+        this.velocityY += this.gravity * dt;
+        this.mesh.position.y += this.velocityY * dt;
+
+        // Collision simple avec le sol
+        if (this.mesh.position.y <= 0.5) { // 0.5 = moitié du cube
+            this.mesh.position.y = 0.5;
+            this.velocityY = 0;
+            this.isGrounded = true;
+        }
+    }   
 }
